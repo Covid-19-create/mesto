74980d66f0b9f4374694e0e52f866494f1df571d
Alexander <bugay9@mail.ru>
2020-05-17 13:12:42 +0400

    Проектная 4 Ошибки

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const body = document.querySelector('.root')
-const editButton = body.querySelector('.profile__editButton')
-const popupClose = body.querySelector('.popup__close')
-const popupSave = body.querySelector('.popup__inputSave')
-const popupName = body.querySelector('.popup__name')
-const popupJob = body.querySelector('.popup__job')
-const fullname = body.querySelector('.profile__fullname')
-const jobs = body.querySelector('.profile__jobs')
-const popup = body.querySelector('.popup')
-const formSubmit = body.querySelector('.form')
-
-function openPopup() {
-    popup.classList.add('popup_opened');
-    popupName.value = fullname.textContent
-    popupJob.value = jobs.textContent
-}
-
-editButton.addEventListener('click', openPopup);
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-
-popupClose.addEventListener('click', closePopup);
-
-function saveSubmit (evt) {
-    evt.preventDefault();
-    fullname.textContent = popupName.value
-    jobs.textContent = popupJob.value
-    closePopup()
-}
-
-formSubmit.addEventListener('submit', saveSubmit)
+const body = document.querySelector('.root') 
+const editButton = body.querySelector('.profile__edit-Button') 
+const popupClose = body.querySelector('.popup__close') 
+const popupSave = body.querySelector('.popup__input-save') 
+const popupName = body.querySelector('.popup__field_name')
+const popupJob = body.querySelector('.popup__field_job')
+const fullname = body.querySelector('.profile__full-name') 
+const jobs = body.querySelector('.profile__jobs') 
+const popup = body.querySelector('.popup') 
+const formSubmit = body.querySelector('.popup__form')
+ 
+function openPopup() { 
+    popup.classList.add('popup_opened'); 
+    popupName.value = fullname.textContent 
+    popupJob.value = jobs.textContent 
+} 
+ 
+editButton.addEventListener('click', openPopup); 
+ 
+function closePopup() { 
+    popup.classList.remove('popup_opened'); 
+} 
+ 
+popupClose.addEventListener('click', closePopup); 
+ 
+function saveSubmit (evt) { 
+    evt.preventDefault(); 
+    fullname.textContent = popupName.value 
+    jobs.textContent = popupJob.value 
+    closePopup() 
+} 
+ 
+formSubmit.addEventListener('submit', saveSubmit) 
