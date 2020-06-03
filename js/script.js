2c56c821f9e6f597ab5fa81a87d1372f7197b197
Alexander <bugay9@mail.ru>
2020-06-03 18:25:31 +0400

    Проектная 5 Исправление final2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const initialPlaces = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
@@ -38,13 +38,13 @@ const formSubmit = body.querySelector('.popup__form')
 const addButton = body.querySelector('.profile__add-Button')
 const places = body.querySelector('.elements') // общая карточка для мини карточек
 const elementTemplate = body.querySelector('#element-template').content //шаблон карты
-const inputNamePic = body.querySelector('.popup__field_picture') // поле ввода имя фотографии
-const inputUrl = body.querySelector('.popup__field_url')// поле ввода Url адреса
+const inputNamePic = popupPlace.querySelector('.popup__field_picture') // поле ввода имя фотографии
+const inputUrl = popupPlace.querySelector('.popup__field_url')// поле ввода Url адреса
 const createNewCard = body.querySelector('.popup__form_place')
 const windowImage = body.querySelector('.popup__window-image') // окно фотографии
 const windowText = body.querySelector('.popup__text')  //окно текста
 const windowPopup = body.querySelector('.popup_window') //popup окно
-
+const popup = body.querySelectorAll('.popup')
 
 function openPopup(popup) { //открытие popup
   popup.classList.add('popup_opened');
@@ -58,9 +58,10 @@ function showPopupProfile() {
 }
 
 
-function closePopup(popup) { //закрытие popup
-  if (popup.target) popup = popup.target.closest('.popup_opened')
-  popup.classList.remove('popup_opened');
+function closePopup() { //закрытие popup 
+  popup.forEach(function (event) {
+    event.classList.remove('popup_opened');
+  })
 }
 
 
@@ -86,8 +87,8 @@ function newPlaceCard(evt) { // создание карточек
     name: inputNamePic.value,
     link: inputUrl.value
   }
-  const PlaceCard = createPlaceCard(place)
-  places.prepend(PlaceCard)
+  const placeCard = createPlaceCard(place)
+  places.prepend(placeCard)
   closePopup(popupPlace)
 }
 
@@ -95,7 +96,7 @@ function newPlaceCard(evt) { // создание карточек
 
 
 
-function createPlaceCard({name, link}) { // 6 карточек из коробки
+function createPlaceCard({ name, link }) { // 6 карточек из коробки
   const placeCard = elementTemplate.cloneNode(true)
   const placeName = placeCard.querySelector('.element__place')
   const placeImage = placeCard.querySelector('.element__image')
@@ -103,33 +104,33 @@ function createPlaceCard({name, link}) { // 6 карточек из коробк
   placeName.textContent = name
   placeImage.dataset.name = name
   placeImage.dataset.link = link
-  placeCard.querySelector('.element__heart').addEventListener('click', function (evt) {     
+  placeCard.querySelector('.element__heart').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__heart_like');
-});
-  
-  placeCard.querySelector('.element__delete').addEventListener('click', function (evt) {    
+  });
+
+  placeCard.querySelector('.element__delete').addEventListener('click', function (evt) {
     evt.target.closest('.element').remove();
-});
-  
-  placeImage.addEventListener('click', function () {                                           
+  });
+
+  placeImage.addEventListener('click', function () {
     windowImage.src = link;
     windowText.textContent = name;
     openPopup(windowPopup);
-});
+  });
   return placeCard
 }
-  
-  
+
+
 function addCardPlaces({ name, link }) { //добавление карточки .elements
-    const card = createPlaceCard({ name, link })
-    places.append(card)
+  const card = createPlaceCard({ name, link })
+  places.append(card)
 }
-  
-  
+
+
 function cardOutput() { //Вывод карточек
-    initialPlaces.forEach(function(event) {
-      addCardPlaces(event)
-    })
+  initialPlaces.forEach(function (event) {
+    addCardPlaces(event)
+  })
 }
 
 cardOutput()
@@ -138,6 +139,4 @@ createNewCard.addEventListener('submit', newPlaceCard)
 formSubmit.addEventListener('submit', saveSubmit);
 editButton.addEventListener('click', showPopupProfile);
 addButton.addEventListener('click', openAddPopup);
-popupClose.forEach(function (event) { 
-  event.addEventListener('click', closePopup)
-});
+popupClose.forEach(event => event.addEventListener('click', closePopup))
